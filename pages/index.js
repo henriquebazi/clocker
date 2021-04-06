c977e5b5c7d379541a302e709f2efa178f259c0a
henriquebazi <henriquebazi96@gmail.com>
2021-04-06 18:42:28 -0300

    feat: redirect user logged user

--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,32 @@
-import { useFormik } from 'formik'
-import * as yup from 'yup'
+import { useEffect, useState } from 'react'
+import { Container, Spinner } from '@chakra-ui/react'
 
-import { Container, Box, Input, Button, Text, FormControl, FormLabel, FormHelperText, Link } from '@chakra-ui/react'
-
-import { Logo } from '../components'
-import firebase, { persistenceMode } from '../config/firebase'
-import 'firebase/auth'
-
-const validationSchema = yup.object().shape({
-  email: yup.string().email('E-mail inválido').required('Preenchimento obrigatório'),
-  password: yup.string().required('Preenchimento obrigatório'),
-})
+import { Login, Agenda } from './../components'
+import firebase from './../config/firebase'
 
 export default function Home() {
-  const formik = useFormik({
-    onSubmit: async (values, form) => {
-      firebase.auth().setPersistence(persistenceMode)
-
-      try {
-        const user = await firebase.auth().signInWithEmailAndPassword(values.email, values.password)
-        
-        console.log(user)
-      } catch (error) {
-        console.log(error)
-      }
-    },
-    validationSchema,
-    initialValues: {
-      email: '',
-      password: ''
-    }
+  const [auth, setAuth] = useState({
+    loading: true,
+    user: false
   })
 
-  return (
-    <Container p={4} centerContent>
-      <Logo />
-      <Box>
-        <Text p={4} mt={8}>
-          Crie sua agenda compartilhada
-        </Text>
-      </Box>
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged( user => {
+      setAuth({
+        loading: false,
+        user
+      })
+    })
+  }, [])
 
-    <Box>
-      <FormControl id="email" p={4} isRequired>
-        <FormLabel>E-mail</FormLabel>
-        <Input 
-          
-          type="email" 
-          value={formik.values.email} 
-          onChange={formik.handleChange} 
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.email && <FormHelperText textColor="#e74c3c">
-          {formik.errors.email}
-        </FormHelperText>}
-      </FormControl>
+  if(auth.loading) {
+    return (
+      <Container p={4} centerContent>
+        <Spinner />
+      </Container>
+    )
+  }
 
-      <FormControl id="password" p={4} isRequired>
-        <FormLabel>Senha</FormLabel>
-        <Input 
-          
-          type="password" 
-          value={formik.values.password} 
-          onChange={formik.handleChange} 
-          onBlur={formik.handleBlur}
-        />
-        {formik.touched.password && <FormHelperText textColor="#e74c3c">
-          {formik.errors.password}
-        </FormHelperText>}
-      </FormControl>
-
-      <Box p={4}>
-        <Button 
-          width="100%" 
-          onClick={formik.handleSubmit} 
-          isLoading={formik.isSubmitting}
-          colorScheme="blue"
-        > 
-          Entrar
-        </Button>
-      </Box>
-    </Box>
-
-    <Link href="/signup">
-      Ainda não possui uma conta? Cadastre-se
-    </Link>
-    </Container>
-  )
+  return auth.user ? <Agenda /> : <Login />
 }
+
