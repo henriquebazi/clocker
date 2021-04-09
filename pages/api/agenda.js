b55f48a730f7d3b52a8e5d7c012d6573ab381ec4
henriquebazi <henriquebazi96@gmail.com>
2021-04-09 14:09:28 -0300

    feat: auth agenda page

--- pages/api/agenda.js
+++ pages/api/agenda.js
@@ -1,5 +1,25 @@
-export default async (req, res) => {
-  console.log(req.query)
+import { firebaseServer } from './../../config/firebase/server'
 
-  res.status(200).json({ name: 'Jany Doe' })
+const database = firebaseServer.firestore()
+const agenda = database.collection('agenda')
+
+export default async (req, res) => {
+  if(!req.headers.authorization) {
+    return res.status(401)
+  }
+
+  try {
+    const [, token] = req.headers.authorization.split(' ')
+    const { user_id } = await firebaseServer.auth().verifyIdToken(token)
+    const snapshot = await agenda
+      .where('userId', '==', user_id)
+      .where('when', '==', req.query.when)
+      .get()
+  
+    return res.status(200).json(snapshot.docs)
+
+  } catch (error) {
+    console.log('FB ERROR:', error)
+    return res.status(401)
+  }
 }
